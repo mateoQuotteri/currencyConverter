1b5b20d4f6f63db0cf022ff6829c3aebe3c8f34b
Mateo Quotteri <quotterimateo@gmail.com>
2023-06-19 13:57:30 -0300

    converting to dolar oficial and blue function already done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const input = document.getElementById("amount");
 const currency = document.getElementById("currency").value
 const form = document.getElementById("form");
-
-
-
+const date = document.getElementById("date")
+const hour = document.getElementById("hour")
+const result = document.getElementById("result")
 
 
 form.addEventListener("submit" , async (e)=>{
@@ -17,9 +17,43 @@ form.addEventListener("submit" , async (e)=>{
            let toDollarBuy = input.value * data.oficial.value_buy;
            let toDollarSell = input.value * data.oficial.value_sell;
 
-           console.log( toDollarBlueSell, toDollarBlueBuy , toDollarBuy, toDollarSell );
+           let resultDolarBlueSell = document.createElement("h5")
+           let resultDolarBlueBuy = document.createElement("h5")
+           let resultDolarSell = document.createElement("h5")
+           let resultDolarBuy = document.createElement("h5")
+           resultDolarBlueSell.textContent = "A dolar blue valor venta: $" + toDollarBlueSell;
+           resultDolarBlueBuy.textContent = "A dolar blue valor compra: $" + toDollarBlueBuy;
+           resultDolarBuy.textContent = "A dolar oficial valor compra: $" + toDollarBuy;
+           resultDolarSell.textContent = "A dolar oficial valor venta: $" + toDollarSell;
+
+           result.appendChild(resultDolarBlueBuy)
+           result.appendChild(resultDolarBlueSell)
+           result.appendChild(resultDolarBuy)
+           result.appendChild(resultDolarSell)
+
+
+
 
         }
   })
    
 })
+
+
+window.onload = function(event) {
+  let today = new Date();
+
+  /*
+  var año = fecha.getFullYear(); // Obtener el año (ej: 2023)
+var mes = fecha.getMonth(); // Obtener el mes (0-11)
+var dia = fecha.getDate(); // Obtener el día del mes (1-31)
+var diaSemana = fecha.getDay(); // Obtener el día de la semana (0-6)
+var hora = fecha.getHours(); // Obtener la hora (0-23)
+var minutos = fecha.getMinutes(); // Obtener los minutos (0-59)
+var segundos = fecha.getSeconds(); // Obtener los segundos (0-59) */
+  
+  date.textContent = today.getDate() + "/" + today.getMonth() + "/" +today.getFullYear();
+  hour.textContent = today.getHours() + ":" + today.getMinutes() + ":" +today.getSeconds()
+
+
+}
