264640e836c2382d7b9bbeaf5a9240d9d3fac1dd
Mateo Quotteri <quotterimateo@gmail.com>
2023-06-20 14:03:08 -0300

    adding euro converter

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
 const input = document.getElementById("amount");
-const currency = document.getElementById("currency").value
+const currency = document.getElementById("currency").textContent
 const form = document.getElementById("form");
 const date = document.getElementById("date")
 const hour = document.getElementById("hour")
 const result = document.getElementById("result")
+function checking(data) {
+  if (currency == "USD") {
+    let toDollarBlueSell = input.value * data.blue.value_sell;
+    let toDollarBlueBuy = input.value * data.blue.value_buy;
+    let toDollarBuy = input.value * data.oficial.value_buy;
+    let toDollarSell = input.value * data.oficial.value_sell;
 
+    let resultDolarBlueSell = document.createElement("h5")
+    let resultDolarBlueBuy = document.createElement("h5")
+    let resultDolarSell = document.createElement("h5")
+    let resultDolarBuy = document.createElement("h5")
+    resultDolarBlueSell.textContent = "A dolar blue valor venta: $" + toDollarBlueSell;
+    resultDolarBlueBuy.textContent = "A dolar blue valor compra: $" + toDollarBlueBuy;
+    resultDolarBuy.textContent = "A dolar oficial valor compra: $" + toDollarBuy;
+    resultDolarSell.textContent = "A dolar oficial valor venta: $" + toDollarSell;
+
+    result.appendChild(resultDolarBlueBuy)
+    result.appendChild(resultDolarBlueSell)
+    result.appendChild(resultDolarBuy)
+    result.appendChild(resultDolarSell)
+
+ }else{
+   console.log("H");
+   let toEuroBlueSell = input.value * data.blue_euro.value_sell;
+    let toEuroBlueBuy = input.value * data.blue_euro.value_buy;
+    let toEuroBuy = input.value * data.oficial_euro.value_buy;
+    let toEuroSell = input.value * data.oficial_euro.value_sell;
+
+    let resultEuroBlueSell = document.createElement("h5")
+    let resultEuroBlueBuy = document.createElement("h5")
+    let resultEuroSell = document.createElement("h5")
+    let resultEuroBuy = document.createElement("h5")
+    resultEuroBlueSell.textContent = "A euro blue valor venta: $" + toEuroBlueSell;
+    resultEuroBlueBuy.textContent = "A euro blue valor compra: $" + toEuroBlueBuy;
+    resultEuroBuy.textContent = "A euro oficial valor compra: $" + toEuroBuy;
+    resultEuroSell.textContent = "A euro oficial valor venta: $" + toEuroSell;
+
+    result.appendChild(resultEuroBlueBuy)
+    result.appendChild(resultEuroBlueSell)
+    result.appendChild(resultEuroBuy)
+    result.appendChild(resultEuroSell)
+ }
+}
 
 form.addEventListener("submit" , async (e)=>{
     e.preventDefault();
     await fetch("https://api.bluelytics.com.ar/v2/latest")
     .then(response => response.json())
     .then(data => {
-        if (currency == "USD") {
-           let toDollarBlueSell = input.value * data.blue.value_sell;
-           let toDollarBlueBuy = input.value * data.blue.value_buy;
-           let toDollarBuy = input.value * data.oficial.value_buy;
-           let toDollarSell = input.value * data.oficial.value_sell;
-
-           let resultDolarBlueSell = document.createElement("h5")
-           let resultDolarBlueBuy = document.createElement("h5")
-           let resultDolarSell = document.createElement("h5")
-           let resultDolarBuy = document.createElement("h5")
-           resultDolarBlueSell.textContent = "A dolar blue valor venta: $" + toDollarBlueSell;
-           resultDolarBlueBuy.textContent = "A dolar blue valor compra: $" + toDollarBlueBuy;
-           resultDolarBuy.textContent = "A dolar oficial valor compra: $" + toDollarBuy;
-           resultDolarSell.textContent = "A dolar oficial valor venta: $" + toDollarSell;
-
-           result.appendChild(resultDolarBlueBuy)
-           result.appendChild(resultDolarBlueSell)
-           result.appendChild(resultDolarBuy)
-           result.appendChild(resultDolarSell)
-
-
-
-
-        }
+      checking(data);
   })
    
 })
